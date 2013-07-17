182635aa426d893259311c197c32ac4391df01e3
bokor <bokor@boky.boky>
2013-07-17 22:26:35 +0200

    - Do not show sql during JUnit tests. - Update version to 1.0.

--- src/test/java/com/boky/SubjectParserTest/springconfig/DaoConfigTest.java
+++ src/test/java/com/boky/SubjectParserTest/springconfig/DaoConfigTest.java
@@ -16,61 +16,61 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
-@ComponentScan(basePackages = {"com.boky.SubjectParserTest"})
+@ComponentScan(basePackages = { "com.boky.SubjectParserTest" })
 public class DaoConfigTest {
 
-    @Value("${jdbc-test.driverClassName}")
-    private String driverClassName;
-    @Value("${jdbc-test.url}")
-    private String url;
-    @Value("${jdbc-test.username}")
-    private String username;
-    @Value("${jdbc-test.password}")
-    private String password;
+	@Value("${jdbc-test.driverClassName}")
+	private String driverClassName;
+	@Value("${jdbc-test.url}")
+	private String url;
+	@Value("${jdbc-test.username}")
+	private String username;
+	@Value("${jdbc-test.password}")
+	private String password;
 
-    @Bean
-    public static PropertyPlaceholderConfigurer properties() {
-        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-        final Resource[] resources = new ClassPathResource[] {new ClassPathResource("database-test.properties")};
-        ppc.setLocations(resources);
+	@Bean
+	public static PropertyPlaceholderConfigurer properties() {
+		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		final Resource[] resources = new ClassPathResource[] { new ClassPathResource("database-test.properties") };
+		ppc.setLocations(resources);
 
-        return ppc;
-    }
+		return ppc;
+	}
 
-    @Bean
-    public DriverManagerDataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
-    }
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(driverClassName);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
 
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setPackagesToScan("com.boky.SubjectParser.daolayer.entities");
-        emf.setDataSource(dataSource());
-        emf.setJpaVendorAdapter(jpaVendorAdapter());
-        emf.setJpaProperties(jpaProperties());
-        return emf;
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setPackagesToScan("com.boky.SubjectParser.daolayer.entities");
+		emf.setDataSource(dataSource());
+		emf.setJpaVendorAdapter(jpaVendorAdapter());
+		emf.setJpaProperties(jpaProperties());
+		return emf;
 
-    }
+	}
 
-    private Properties jpaProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
-        properties.setProperty("hibernate.use_sql_comments", "false");
-        properties.setProperty("hibernate.generate_statistics", "false");
-        return properties;
-    }
+	private Properties jpaProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "create");
+		properties.setProperty("hibernate.use_sql_comments", "false");
+		properties.setProperty("hibernate.generate_statistics", "false");
+		return properties;
+	}
 
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        hibernateJpaVendorAdapter.setShowSql(true);
-        hibernateJpaVendorAdapter.setDatabase(Database.HSQL);
-        return hibernateJpaVendorAdapter;
-    }
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+		hibernateJpaVendorAdapter.setShowSql(false);
+		hibernateJpaVendorAdapter.setDatabase(Database.HSQL);
+		return hibernateJpaVendorAdapter;
+	}
 }
